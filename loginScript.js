dc5b936c8053c94a6f277eb201e2fbce1e7e3019
sov <diegoortega1045@gmail.com>
2025-04-13 23:02:27 -0500

    1

---
+++ loginScript.js
@@ -0,0 +1,3 @@
+document.getElementById('botonR').addEventListener('click', function () {
+    window.open('lRegistro.html', 'popup', 'width=600,height=800', close(window));
+});
---
+++ principalScript.js
@@ -0,0 +1,4 @@
+
+document.getElementById('botonLogin').addEventListener('click', function () {
+    window.open('login.html', 'popup', 'width=800,height=600');
+});
